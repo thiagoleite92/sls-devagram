ef47432023858b110db7ab9fd3b1d6382b156ee2
Thiago Leite <leite.tjs@gmail.com>
2022-10-23 18:21:00 -0300

    update user working

--- src/functions/user.ts
+++ src/functions/user.ts
@@ -1,3 +1,4 @@
+import { FileData } from 'aws-multipart-parser/dist/models';
 import { UserModel } from './../models/UserModel';
 import { APIGatewayEvent, Handler } from 'aws-lambda';
 import { CognitoServices } from '../services/CognitoServices';
@@ -8,6 +9,8 @@ import {
   formatDefaultResponse,
 } from '../utils/formatResponseUtil';
 import { S3Service } from '../services/S3Services';
+import { parse } from 'aws-multipart-parser';
+import { imageAllowedExtensions } from '../constants/Regexes';
 
 export const me: Handler = async (
   event: APIGatewayEvent
@@ -42,3 +45,57 @@ export const me: Handler = async (
     );
   }
 };
+
+export const update: Handler = async (event: APIGatewayEvent): Promise<DefaultJsonResponse> => {
+  try {
+    const { USER_TABLE, AVATAR_BUCKET } = process.env
+    
+if (!USER_TABLE || !AVATAR_BUCKET) {
+  return formatDefaultResponse(500, 'ENVs para serviço não encontradas.')
+} 
+
+const userId = getUserIdFromEvent(event);
+
+  if (!userId) {
+    return formatDefaultResponse(400, 'Usuário não encontrado.')
+  }
+
+  if (!event.body) {
+    return formatDefaultResponse(400, 'Parâmetros de entrada invaĺidos.')
+  }
+  
+  const user = await UserModel.get({'cognitoId': userId})
+
+  const formData = parse(event, true);
+
+  const file = formData.file as FileData;
+  const name = formData.name as string;
+
+  if (name && name.trim().length < 2) {
+    return formatDefaultResponse(400, 'Nome inválido.');
+  } else if (name) {
+    user.name = name;
+  }
+
+
+
+  if (file && !imageAllowedExtensions.exec(file.filename)) {
+    return formatDefaultResponse(
+      400,
+      'Extensão informada do arquivo não é válida'
+    );
+  } else if (file) {
+    const newKey = await new S3Service().saveImage(AVATAR_BUCKET, 'avatar', file)
+
+    user.avatar = newKey
+  }
+
+  await UserModel.update(user)  ;
+
+  return formatDefaultResponse(200, 'Usuário alterado com sucesso!')
+
+  } catch (error) {
+      console.log('Error on update user data: ', error)
+      return formatDefaultResponse(500, 'Erro ao atualizar dados do usuário. Tente novamente ou contacte o administrador do sistema.')
+  }
+}
