2b6571f45fbb7e5d63dee820cd84916ce72a6a5a
Thiago Leite <leite.tjs@gmail.com>
2022-11-16 15:36:52 -0300

    feed by userid and feed pessoal

---
+++ src/functions/feed.ts
@@ -0,0 +1,75 @@
+import { PostModel } from './../models/PostModel';
+import { getUserIdFromEvent } from '../utils/authenticationHandlerUtils';
+import { validateEnvs } from '../utils/environmentsUtils';
+import { Handler } from 'aws-lambda';
+import {
+  DefaultJsonResponse,
+  formatDefaultResponse,
+} from '../utils/formatResponseUtil';
+import { DefaultListPaginatedResponse } from '../types/DefaultListPaginatedResponse';
+import { S3Service } from '../services/S3Services';
+import { UserModel } from '../models/UserModel';
+import { FeedLastKeyRequest } from '../types/feed/FeedLastKeyRequest';
+
+export const findByUserId: Handler = async (
+  event: any
+): Promise<DefaultJsonResponse> => {
+  try {
+    const { error, POST_BUCKET } = validateEnvs([
+      'USER_TABLE',
+      'POST_TABLE',
+      'POST_BUCKET',
+    ]);
+
+    if (error) {
+      return formatDefaultResponse(500, error);
+    }
+
+    const { userId } = event.pathParameters || {
+      userId: getUserIdFromEvent(event),
+    };
+
+    if (!userId) {
+      return formatDefaultResponse(400, 'Usuário não encontrado.');
+    }
+
+    const user = await UserModel.get({ cognitoId: userId });
+
+    if (!user) {
+      return formatDefaultResponse(400, 'Usuário não encontrado.');
+    }
+
+    const lastKey = (event.queryStringParameters || null) as FeedLastKeyRequest;
+
+    const query = PostModel.query({ userId }).sort('descending');
+
+    if (lastKey && lastKey.id && lastKey.userId && lastKey.date) {
+      query.startAt(lastKey);
+    }
+
+    const result = await query.limit(5).exec();
+
+    const response = {} as DefaultListPaginatedResponse;
+
+    if (result) {
+      response.count = result.count;
+      response.lastKey = response.lastKey;
+
+      for (const document of result) {
+        if (document && document.image) {
+          document.image = await new S3Service().getImageUrl(
+            POST_BUCKET,
+            document.image
+          );
+        }
+      }
+
+      response.data = result;
+    }
+
+    return formatDefaultResponse(200, undefined, response);
+  } catch (error) {
+    console.log('Error on follow/unfollow user.', error);
+    return formatDefaultResponse(500, 'Error ao seguir/deseguir usuário');
+  }
+};
